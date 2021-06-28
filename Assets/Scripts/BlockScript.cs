1976dc5413ef6ad89f2a23ef991d200b3b9eb9ce
BJNicholas <85209940+BJNicholas@users.noreply.github.com>
2021-06-28 12:39:54 +1000

    Fixed demon block spawn

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class BlockScript : MonoBehaviour
 {
-    private void Start()
+    [Range(1,10)]
+    public int decoyChance;
+    public bool isDecoy = false;
+
+    public GameObject demonBox;
+    private void Awake()
     {
-       
+        int roll = Random.Range(0, decoyChance);
+        if(roll == 0)
+        {
+            isDecoy = true;
+        }
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (isDecoy)
+            {
+                GameObject spawnedBox = Instantiate(demonBox, transform.position, transform.rotation);
+                Destroy(gameObject);
+
+                GameManager.instance.health -= 10;
+            }
+        }
     }
 }
