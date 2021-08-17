960d2fa969af2ba3f3454d45cc24eaa280fc7cd2
AlecDubouzet <85209846+wrobe71279@users.noreply.github.com>
2021-08-17 16:37:28 +1000

    Minor Animation Fix
    - Updated CharacterAnimation Script to trigger Hurt animation when in contact with Ember

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -14,6 +14,15 @@ public class CharacterAnimation : MonoBehaviour
     }
 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            anim.SetTrigger("takedamage");
+        }
+
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
